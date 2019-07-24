879a0d9d143891e9a74c45d591d20f40d34ec252
zhaobw <1964471757@qq.com>
2019-07-24 15:47:09 +0800

    初始化头部

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -34,8 +34,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'src':resolve('src'),
-      'common':resolve('src/common')
+      '@': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'base': resolve('src/base'),
+      'api': resolve('src/api')
     }
   },
   module: {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
 
+fastclick.attach(document.body)
 
 new Vue({
     el:'#app',
