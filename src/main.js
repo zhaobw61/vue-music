e43560215c27c4164706d6d48c621e7009055eb9
zhaobw <1964471757@qq.com>
2019-07-24 16:18:57 +0800

    初始化路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 import fastclick from 'fastclick'
 
@@ -10,5 +11,6 @@ fastclick.attach(document.body)
 
 new Vue({
     el:'#app',
-    render:h => h(App)
+    render:h => h(App),
+    router
 })
