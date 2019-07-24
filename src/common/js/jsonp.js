5c3a4c6b44591c3d35e4270a6c73350b95fe9c99
zhaobw <1964471757@qq.com>
2019-07-24 17:44:43 +0800

    完成JSONP封装

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJSONP from 'jsonp'
+
+export default function(url,data,options){
+
+    url += (url.indexof('?') < 0 ? '?' : '&') + para(data);
+    return new promise((reslove,reject)=>{
+        originJSONP(url,options,(err,data)=>{
+            if(!err){
+                reslove(data);
+            }else{
+                reject(err);
+            }
+        })
+    })
+}
+
+function para(data){
+    let url = '';
+    for(var key in data){
+        let value = data[key] !== undefined ? data[key] : '';
+        url += `&${key}=${encodeURIComponent(value)}`;
+    }
+    return url ? url.substring(1) : "";
+}
